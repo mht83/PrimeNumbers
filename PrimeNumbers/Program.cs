f93e9978e24523e44025d66124a87c0ef6642437
Mohammadhassan Tohidi <tohidihassan9@gmail.com>
2024-03-26 14:56:38 +0330

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class PrimeNumbers
+{
+    static void Main()
+    {
+        const int limit = 10000;
+        for (int number = 2; number < limit; number++)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+
+   
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        var boundary = (int)Math.Sqrt(number);
+
+        for (int i = 3; i <= boundary; i += 2)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
